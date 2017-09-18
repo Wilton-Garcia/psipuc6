1ccb1fba34866b9fb18a5575b5b22be8f7dc7306
Wilton-Garcia <wiltongarciaweb@gmail.com>
2017-09-17 21:34:37 -0300

    Revert "Revert "Commit 007 - Wilton""
    This reverts commit bc727c964df2dae6d95ff55c2a49a0944726a948.

--- psipuc/src/java/puc/si/psi/ti/model/Instituicao.java
+++ psipuc/src/java/puc/si/psi/ti/model/Instituicao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package puc.psi.ti.model;
+package puc.si.psi.ti.model;
 
 import java.util.List;
 
